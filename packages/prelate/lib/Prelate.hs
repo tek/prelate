09ac9bfb4186ca457d08c63ddd4af8a929a8bc0e
Torsten Schmits <git@tryp.io>
2022-07-07 01:31:51 +0200

    remove some lens imports

--- packages/prelate/lib/Prelate.hs
+++ packages/prelate/lib/Prelate.hs
@@ -5,7 +5,6 @@ module Prelate (
   module Prelate.Json,
   module Data.Aeson,
   module Lens.Micro,
-  module Lens.Micro.Extras,
   module Lens.Micro.GHC,
 ) where
 
@@ -13,8 +12,7 @@ import Data.Aeson (FromJSON, ToJSON)
 import Data.Generics.Labels ()
 import Incipit
 import Lens.Micro hiding (lens)
-import Lens.Micro.Extras
-import Lens.Micro.GHC hiding (lens)
+import Lens.Micro.GHC (at)
 
 import Prelate.Atomic
 import Prelate.Json
