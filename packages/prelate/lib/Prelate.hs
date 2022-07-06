69f31becf7cbf1f5d3dec45fe44746a2069fd1b1
Torsten Schmits <git@tryp.io>
2022-07-07 01:10:12 +0200

    add alias @@ for Tagged

--- packages/prelate/lib/Prelate.hs
+++ packages/prelate/lib/Prelate.hs
@@ -1,38 +1,24 @@
 module Prelate (
   module Incipit,
+  module Prelate,
   module Prelate.Atomic,
+  module Prelate.Json,
   module Data.Aeson,
   module Lens.Micro,
   module Lens.Micro.Extras,
   module Lens.Micro.GHC,
-  json,
-  unaryJson,
 ) where
 
-import qualified Data.Aeson as Aeson
 import Data.Aeson (FromJSON, ToJSON)
-import Data.Aeson.TH (deriveJSON)
 import Data.Generics.Labels ()
-import Data.List (dropWhileEnd)
 import Incipit
-import qualified Language.Haskell.TH.Syntax as TH
 import Lens.Micro hiding (lens)
 import Lens.Micro.Extras
 import Lens.Micro.GHC hiding (lens)
 
 import Prelate.Atomic
+import Prelate.Json
 
--- |Aeson codec options that remove leading and trailing underscores.
-basicOptions :: Aeson.Options
-basicOptions =
-  Aeson.defaultOptions { Aeson.fieldLabelModifier = dropWhileEnd ('_' ==) . dropWhile ('_' ==) }
-
--- |Derive Aeson codecs that strip underscores and unwrap data/newtype with single fields.
-json :: TH.Name -> TH.Q [TH.Dec]
-json =
-  deriveJSON basicOptions { Aeson.unwrapUnaryRecords = True }
-
--- |Derive Aeson codecs that strip underscores.
-unaryJson :: TH.Name -> TH.Q [TH.Dec]
-unaryJson =
-  deriveJSON basicOptions
+-- |Alias for 'Tagged'.
+type a @@ b =
+  Tagged b a
---
+++ packages/prelate/lib/Prelate/Json.hs
@@ -0,0 +1,26 @@
+module Prelate.Json (
+  json,
+  unaryJson,
+) where
+
+import qualified Data.Aeson as Aeson
+import Data.Aeson.TH (deriveJSON)
+import Data.Generics.Labels ()
+import Data.List (dropWhileEnd)
+import qualified Language.Haskell.TH.Syntax as TH
+
+
+-- |Aeson codec options that remove leading and trailing underscores.
+basicOptions :: Aeson.Options
+basicOptions =
+  Aeson.defaultOptions { Aeson.fieldLabelModifier = dropWhileEnd ('_' ==) . dropWhile ('_' ==) }
+
+-- |Derive Aeson codecs that strip underscores and unwrap data/newtype with single fields.
+json :: TH.Name -> TH.Q [TH.Dec]
+json =
+  deriveJSON basicOptions { Aeson.unwrapUnaryRecords = True }
+
+-- |Derive Aeson codecs that strip underscores.
+unaryJson :: TH.Name -> TH.Q [TH.Dec]
+unaryJson =
+  deriveJSON basicOptions
