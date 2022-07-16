c0552668346b2a6f6ac4395b1461166f18a56b57
Torsten Schmits <git@tryp.io>
2022-07-16 18:44:53 +0200

    allow importing prelude even when hiding the mixin

--- packages/prelate/lib/Prelate.hs
+++ packages/prelate/lib/Prelate.hs
@@ -1,22 +1,3 @@
-module Prelate (
-  module Incipit,
-  module Prelate,
-  module Prelate.Atomic,
-  module Prelate.Json,
-  module Data.Aeson,
-  module Lens.Micro,
-  module Lens.Micro.GHC,
-) where
+module Prelate (module Prelate.Prelude) where
 
-import Data.Aeson (FromJSON, ToJSON)
-import Data.Generics.Labels ()
-import Incipit
-import Lens.Micro hiding (lens)
-import Lens.Micro.GHC (at)
-
-import Prelate.Atomic
-import Prelate.Json
-
--- |Alias for 'Tagged'.
-type a @@ b =
-  Tagged b a
+import Prelate.Prelude
---
+++ packages/prelate/lib/Prelate/Prelude.hs
@@ -0,0 +1,22 @@
+module Prelate.Prelude (
+  module Incipit,
+  module Prelate.Prelude,
+  module Prelate.Atomic,
+  module Prelate.Json,
+  module Data.Aeson,
+  module Lens.Micro,
+  module Lens.Micro.GHC,
+) where
+
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Generics.Labels ()
+import Incipit
+import Lens.Micro hiding (lens)
+import Lens.Micro.GHC (at)
+
+import Prelate.Atomic
+import Prelate.Json
+
+-- |Alias for 'Tagged'.
+type a @@ b =
+  Tagged b a
