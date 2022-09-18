450c3e0500613046ee7923e19306fad0ad964f80
Torsten Schmits <git@tryp.io>
2022-09-18 18:39:29 +0200

    bump deps

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "A Prelude";
 
   inputs = {
-    hix.url = github:tek/hix;
-    incipit.url = github:tek/incipit;
+    hix.url = git+https://git.tryp.io/tek/hix;
+    incipit.url = git+https://git.tryp.io/tek/incipit;
   };
 
   outputs = { hix, incipit, ... }:
@@ -14,6 +14,7 @@
   in hix.lib.flake ({ config, lib, ... }: {
     base = ./.;
     packages.prelate = ./packages/prelate;
+    devGhc.compiler = "ghc902";
     overrides = { inherit all; };
     depsFull = [incipit];
     hackage.versionFile = "ops/version.nix";
