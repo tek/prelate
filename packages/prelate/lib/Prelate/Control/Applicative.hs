c34fc040f7a26222d9a3dc0b4aa68cb65a8c6372
Torsten Schmits <git@tryp.io>
2022-10-01 13:35:29 +0200

    add pairA

---
+++ packages/prelate/lib/Prelate/Control/Applicative.hs
@@ -0,0 +1,11 @@
+module Prelate.Control.Applicative where
+
+-- |Create a pair from two applicative values.
+pairA ::
+  Applicative f =>
+  f a ->
+  f b ->
+  f (a, b)
+pairA fa fb =
+  (,) <$> fa <*> fb
+{-# inline pairA #-}
