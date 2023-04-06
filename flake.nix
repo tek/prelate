444d001fe322908422d487775e650be555f4c8bd
Torsten Schmits <git@tryp.io>
2023-04-06 23:49:24 +0200

    bump hix

--- flake.nix
+++ flake.nix
@@ -3,29 +3,106 @@
 
   inputs = {
     hix.url = "git+https://git.tryp.io/tek/hix";
-    hls.url = "github:haskell/haskell-language-server?ref=1.9.0.0";
-    incipit.url = "git+https://git.tryp.io/tek/incipit?tag=v0.7.0.0";
+    incipit.url = "git+https://git.tryp.io/tek/incipit";
     exon.url = "git+https://git.tryp.io/tek/exon";
   };
 
-  outputs = { hix, hls, incipit, exon, ... }:
-  let
+  outputs = { hix, incipit, exon, ... }: hix.lib.pro {
+    ghcVersions = ["ghc810" "ghc90" "ghc92" "ghc94"];
+    hackage.versionFile = "ops/version.nix";
+    deps = [exon incipit];
 
-    all = { hackage, jailbreak, unbreak, ... }: {
+    overrides = { hackage, jailbreak, unbreak, ... }: {
       exon = hackage "1.4.0.0" "1m4i3a14wip985ncblfy2ikcy7gw5rryj9z497ah218d1nmwj7rl";
-      incipit-base = hackage "0.5.0.0" "02fdppamn00m94xqi4zhm6sl1ndg6lhn24m74w24pq84h44mynl6";
-      incipit-core = hackage "0.5.0.0" "1pql8s941jb21kvsx5py4ffnilm1ga136npa25ifsh3l7yapci30";
       incipit = hackage "0.7.0.0" "1gwca88qldfn7qmx0wmpcpasr8qajyppijj8ahif8lav3pf37gvn";
       zeugma = hackage "0.7.0.0" "150vrib454a8vssrndi0xfdfg1zrlkfg6yi6jjjbhpmkimf80wvs";
     };
 
-  in hix.lib.pro ({ config, lib, ... }: {
-    packages.prelate = ./packages/prelate;
-    devGhc.compiler = "ghc925";
-    overrides = { inherit all; };
-    deps = [incipit exon];
-    hackage.versionFile = "ops/version.nix";
-    hpack.packages = import ./ops/hpack.nix { inherit config lib; };
-    shell.hls.package = hls.packages.${config.system}.haskell-language-server-925;
-  });
+    cabal = {
+      license = "BSD-2-Clause-Patent";
+      license-file = "LICENSE";
+      author = "Torsten Schmits";
+      meta = {
+        maintainer = "hackage@tryp.io";
+        category = "Prelude";
+        github = "tek/prelate";
+        extra-source-files = ["readme.md"];
+      };
+    };
+
+    packages.prelate = {
+      src = ./packages/prelate;
+
+      cabal.meta.synopsis = "A Prelude";
+
+      library = {
+        enable = true;
+        dependencies = [
+          "aeson >= 2.0"
+          "base >= 4.13 && < 4.18"
+          "extra ^>= 1.7.10"
+          "exon ^>= 1.4"
+          "generic-lens >= 2.2"
+          "incipit ^>= 0.7"
+          "microlens ^>= 0.4"
+          "microlens-ghc ^>= 0.4"
+          "polysemy-chronos ^>= 0.6"
+          "polysemy-conc ^>= 0.12"
+          "polysemy-log ^>= 0.9"
+          "polysemy-process ^>= 0.12"
+          "polysemy-resume ^>= 0.7"
+          "polysemy-time ^>= 0.6"
+          "template-haskell"
+        ];
+        component.reexported-modules = [
+          "Control.Concurrent.STM"
+          "Control.Concurrent.STM.TArray"
+          "Control.Concurrent.STM.TBQueue"
+          "Control.Concurrent.STM.TChan"
+          "Control.Concurrent.STM.TMVar"
+          "Control.Concurrent.STM.TQueue"
+          "Control.Concurrent.STM.TSem"
+          "Control.Concurrent.STM.TVar"
+          "Control.Monad.STM"
+          "Data.ByteString"
+          "Data.ByteString.Builder"
+          "Data.ByteString.Lazy"
+          "Data.ByteString.Short"
+          "Data.IntMap.Lazy"
+          "Data.IntMap.Strict"
+          "Data.IntSet"
+          "Data.Map.Lazy"
+          "Data.Map.Strict"
+          "Data.Sequence"
+          "Data.Set"
+          "Data.Text"
+          "Data.Text.IO"
+          "Data.Text.Lazy"
+          "Data.Text.Lazy.Builder"
+          "Data.Text.Lazy.IO"
+          "Data.Text.Read"
+          "Data.Tree"
+          "Exon"
+          "Lens.Micro"
+          "Lens.Micro.Extras"
+          "Lens.Micro.GHC"
+          "Polysemy.Chronos"
+          "Polysemy.Conc"
+          "Polysemy.Conc.Queue"
+          "Polysemy.Conc.Sync"
+          "Polysemy.Log"
+          "Polysemy.Process"
+          "Polysemy.Resume"
+          "Polysemy.Time"
+          "Conc"
+          "Log"
+          "Queue"
+          "Sync"
+          "Time"
+        ];
+      };
+
+    };
+
+  };
 }
--- ops/hpack.nix
+++ ops/hpack.nix
@@ -3,124 +3,12 @@ with builtins;
 with lib;
 let
 
-  mergeAttr = a: b:
-  if isAttrs a
-  then merge a b
-  else if isList a
-  then a ++ b
-  else b;
-
-  merge = l: r:
-  let
-    f = name:
-    if hasAttr name l && hasAttr name r
-    then mergeAttr l.${name} r.${name}
-    else l.${name} or r.${name};
-  in genAttrs (concatMap attrNames [l r]) f;
-
-  paths = name: {
-    when = {
-      condition = false;
-      generated-other-modules = ["Paths_${replaceStrings ["-"] ["_"] name}"];
-    };
-  };
-
-  meta = {
-    version = import ./version.nix;
-    license = "BSD-2-Clause-Patent";
-    license-file = "LICENSE";
-    author = "Torsten Schmits";
-    maintainer = "hackage@tryp.io";
-    copyright = "2022 Torsten Schmits";
-    category = "Prelude";
-    build-type = "Simple";
-    github = "tek/prelate";
-  };
-
-  options.ghc-options = [
-    "-Wall"
-    "-Wredundant-constraints"
-    "-Wincomplete-uni-patterns"
-    "-Wmissing-deriving-strategies"
-    "-Widentities"
-    "-Wunused-packages"
-  ];
-
-  project = name: doc: merge (meta // { library = paths name; } // options) {
-    inherit name;
-    description = "See https://hackage.haskell.org/package/${name}/docs/${doc}.html";
-    library.source-dirs = "lib";
-    default-extensions = config.ghci.extensions;
-  };
-
-  reexported-modules = [
-    "Control.Concurrent.STM"
-    "Control.Concurrent.STM.TArray"
-    "Control.Concurrent.STM.TBQueue"
-    "Control.Concurrent.STM.TChan"
-    "Control.Concurrent.STM.TMVar"
-    "Control.Concurrent.STM.TQueue"
-    "Control.Concurrent.STM.TSem"
-    "Control.Concurrent.STM.TVar"
-    "Control.Monad.STM"
-    "Data.ByteString"
-    "Data.ByteString.Builder"
-    "Data.ByteString.Lazy"
-    "Data.ByteString.Short"
-    "Data.IntMap.Lazy"
-    "Data.IntMap.Strict"
-    "Data.IntSet"
-    "Data.Map.Lazy"
-    "Data.Map.Strict"
-    "Data.Sequence"
-    "Data.Set"
-    "Data.Text"
-    "Data.Text.IO"
-    "Data.Text.Lazy"
-    "Data.Text.Lazy.Builder"
-    "Data.Text.Lazy.IO"
-    "Data.Text.Read"
-    "Data.Tree"
-    "Exon"
-    "Lens.Micro"
-    "Lens.Micro.Extras"
-    "Lens.Micro.GHC"
-    "Polysemy.Chronos"
-    "Polysemy.Conc"
-    "Polysemy.Conc.Queue"
-    "Polysemy.Conc.Sync"
-    "Polysemy.Log"
-    "Polysemy.Process"
-    "Polysemy.Resume"
-    "Polysemy.Time"
-    "Conc"
-    "Log"
-    "Queue"
-    "Sync"
-    "Time"
-  ];
-
 in {
 
   prelate = merge (project "prelate" "Prelate") {
     synopsis = "A Prelude";
     library = {
       dependencies = [
-        "aeson >= 2.0"
-        "base >= 4.13 && < 4.18"
-        "extra ^>= 1.7.10"
-        "exon ^>= 1.4"
-        "generic-lens >= 2.2"
-        "incipit ^>= 0.7"
-        "microlens ^>= 0.4"
-        "microlens-ghc ^>= 0.4"
-        "polysemy-chronos ^>= 0.6"
-        "polysemy-conc ^>= 0.12"
-        "polysemy-log ^>= 0.9"
-        "polysemy-process ^>= 0.12"
-        "polysemy-resume ^>= 0.7"
-        "polysemy-time ^>= 0.6"
-        "template-haskell"
       ];
       inherit reexported-modules;
     };
