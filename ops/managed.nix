16af04feb8196eb4cf151da95fa220d5e08b99ed
tek <2632+tek@users.noreply.github.com>
2024-05-31 23:40:50 +0000

    Bump 15 dependencies
    New versions:
    
    * aeson-2.2.1.0 [>=2.1.0.0 && <2.3]
    * base-4.19.1.0 [>=4.17.2.1 && <4.20]
    * exon-1.7.0.0 [>=1.4.0.0 && <1.8]
    * extra-1.7.16 [>=1.7.9 && <1.8]
    * generic-lens-2.2.2.0 [>=2.2.1.0 && <2.3]
    * incipit-0.10.0.0 [>=0.7.0.0 && <0.11]
    * microlens-0.4.13.1 [>=0.4.12.0 && <0.5]
    * microlens-ghc-0.4.14.3 [>=0.4.13.1 && <0.5]
    * polysemy-chronos-0.6.0.2 [>=0.5.0.0 && <0.7]
    * polysemy-conc-0.13.0.1 [>=0.12.1.0 && <0.14]
    * polysemy-log-0.11.0.0 [>=0.9.0.0 && <0.12]
    * polysemy-process-0.13.0.1 [>=0.12.0.0 && <0.14]
    * polysemy-resume-0.8.0.1 [>=0.7.0.0 && <0.9]
    * polysemy-time-0.6.0.2 [>=0.6.0.0 && <0.7]
    * template-haskell-2.21.0.0 [>=2.19.0.0 && <2.22]

--- ops/managed.nix
+++ ops/managed.nix
@@ -3,68 +3,84 @@
     prelate = {
       aeson = {
         lower = "2.1.0.0";
-        upper = null;
+        upper = "2.3";
       };
       base = {
         lower = "4.17.2.1";
-        upper = null;
+        upper = "4.20";
       };
       exon = {
         lower = "1.4.0.0";
-        upper = null;
+        upper = "1.8";
       };
       extra = {
         lower = "1.7.9";
-        upper = null;
+        upper = "1.8";
       };
       generic-lens = {
         lower = "2.2.1.0";
-        upper = null;
+        upper = "2.3";
       };
       incipit = {
         lower = "0.7.0.0";
-        upper = null;
+        upper = "0.11";
       };
       microlens = {
         lower = "0.4.12.0";
-        upper = null;
+        upper = "0.5";
       };
       microlens-ghc = {
         lower = "0.4.13.1";
-        upper = null;
+        upper = "0.5";
       };
       polysemy-chronos = {
         lower = "0.5.0.0";
-        upper = null;
+        upper = "0.7";
       };
       polysemy-conc = {
         lower = "0.12.1.0";
-        upper = null;
+        upper = "0.14";
       };
       polysemy-log = {
         lower = "0.9.0.0";
-        upper = null;
+        upper = "0.12";
       };
       polysemy-process = {
         lower = "0.12.0.0";
-        upper = null;
+        upper = "0.14";
       };
       polysemy-resume = {
         lower = "0.7.0.0";
-        upper = null;
+        upper = "0.9";
       };
       polysemy-time = {
         lower = "0.6.0.0";
-        upper = null;
+        upper = "0.7";
       };
       template-haskell = {
         lower = "2.19.0.0";
-        upper = null;
+        upper = "2.22";
       };
     };
   };
   versions = {
-    latest = {};
+    latest = {
+      aeson = "2.2.1.0";
+      base = "4.19.1.0";
+      exon = "1.7.0.0";
+      extra = "1.7.16";
+      generic-lens = "2.2.2.0";
+      incipit = "0.10.0.0";
+      microlens = "0.4.13.1";
+      microlens-ghc = "0.4.14.3";
+      polysemy-chronos = "0.6.0.2";
+      polysemy-conc = "0.13.0.1";
+      polysemy-log = "0.11.0.0";
+      polysemy-process = "0.13.0.1";
+      polysemy-resume = "0.8.0.1";
+      polysemy-time = "0.6.0.2";
+      template-haskell = "2.21.0.0";
+    };
     lower = {
       aeson = "2.1.0.0";
       base = "4.17.2.1";
@@ -102,7 +118,28 @@
     };
   };
   overrides = {
-    latest = {};
+    latest = {
+      exon = {
+        version = "1.7.0.0";
+        hash = "142i8ka6b16ydllhhb2305ml3hij66h6y555fp6cvc82166kdrhb";
+      };
+      extra = {
+        version = "1.7.16";
+        hash = "0y27q0zas66qbgvjga0w8dmfjcs0kcn9nwps17iyd60ij3yqivhj";
+      };
+      incipit = {
+        version = "0.10.0.0";
+        hash = "1r3y2wp8wz1ii28a6wb76z6w3sgiah158kwsadrr13w6iryhq047";
+      };
+      microlens-ghc = {
+        version = "0.4.14.3";
+        hash = "0pivw9yrsbfand99kqx8rmll0653k2fzymd6y8av44ky4j3pydga";
+      };
+      polysemy-log = {
+        version = "0.11.0.0";
+        hash = "1phzwj2ig0vx1anscg3qv53ysa0f7gks37pc8gfkh2aws3qp6cda";
+      };
+    };
     lower = {
       OneTuple = {
         version = "0.3.1";
--- ops/overrides.nix
+++ ops/overrides.nix
@@ -10,6 +10,133 @@ ghc98 = {
 hls = {
 };
 latest = {
+  exon = {
+  meta = {
+    sha256 = "142i8ka6b16ydllhhb2305ml3hij66h6y555fp6cvc82166kdrhb";
+    ver = "1.7.0.0";
+  };
+  drv = { mkDerivation, base, criterion, ghc, hedgehog, incipit-base, lib
+, parsec, tasty, tasty-hedgehog, template-haskell
+}:
+mkDerivation {
+  pname = "exon";
+  version = "1.7.0.0";
+  src = /nix/store/p3qg36znxvgfmg2vj3gbcsq1m92l6j2s-source;
+  libraryHaskellDepends = [
+    base ghc incipit-base parsec template-haskell
+  ];
+  testHaskellDepends = [
+    base hedgehog incipit-base tasty tasty-hedgehog template-haskell
+  ];
+  benchmarkHaskellDepends = [ base criterion incipit-base ];
+  homepage = "https://github.com/tek/exon#readme";
+  description = "Customizable quasiquote interpolation";
+  license = "BSD-2-Clause-Patent";
+}
+;
+}
+;
+  extra = {
+  meta = {
+    sha256 = "0y27q0zas66qbgvjga0w8dmfjcs0kcn9nwps17iyd60ij3yqivhj";
+    ver = "1.7.16";
+  };
+  drv = { mkDerivation, base, clock, directory, filepath, lib, process
+, QuickCheck, quickcheck-instances, time, unix
+}:
+mkDerivation {
+  pname = "extra";
+  version = "1.7.16";
+  src = /nix/store/kci6vs1ndgbq329dgksjqcx8hvlk31b2-source;
+  libraryHaskellDepends = [
+    base clock directory filepath process time unix
+  ];
+  testHaskellDepends = [
+    base directory filepath QuickCheck quickcheck-instances unix
+  ];
+  homepage = "https://github.com/ndmitchell/extra#readme";
+  description = "Extra functions I use";
+  license = lib.licenses.bsd3;
+}
+;
+}
+;
+  incipit = {
+  meta = {
+    sha256 = "1r3y2wp8wz1ii28a6wb76z6w3sgiah158kwsadrr13w6iryhq047";
+    ver = "0.10.0.0";
+  };
+  drv = { mkDerivation, base, incipit-core, lib, polysemy-conc
+, polysemy-log, polysemy-resume, polysemy-time
+}:
+mkDerivation {
+  pname = "incipit";
+  version = "0.10.0.0";
+  src = /nix/store/vsgx9m5cgsyvd8hvznamavar6ca2q0x8-source;
+  libraryHaskellDepends = [
+    base incipit-core polysemy-conc polysemy-log polysemy-resume
+    polysemy-time
+  ];
+  homepage = "https://github.com/tek/incipit#readme";
+  description = "A Prelude for Polysemy";
+  license = "BSD-2-Clause-Patent";
+}
+;
+}
+;
+  microlens-ghc = {
+  meta = {
+    sha256 = "0pivw9yrsbfand99kqx8rmll0653k2fzymd6y8av44ky4j3pydga";
+    ver = "0.4.14.3";
+  };
+  drv = { mkDerivation, array, base, bytestring, containers, lib, microlens
+, transformers
+}:
+mkDerivation {
+  pname = "microlens-ghc";
+  version = "0.4.14.3";
+  src = /nix/store/dkhyfnp716q9akd0infml476cp7bb8a7-source;
+  libraryHaskellDepends = [
+    array base bytestring containers microlens transformers
+  ];
+  homepage = "http://github.com/stevenfontanella/microlens";
+  description = "microlens + array, bytestring, containers, transformers";
+  license = lib.licenses.bsd3;
+}
+;
+}
+;
+  polysemy-log = {
+  meta = {
+    sha256 = "1phzwj2ig0vx1anscg3qv53ysa0f7gks37pc8gfkh2aws3qp6cda";
+    ver = "0.11.0.0";
+  };
+  drv = { mkDerivation, ansi-terminal, async, base, incipit-core, lib
+, polysemy, polysemy-conc, polysemy-plugin, polysemy-test
+, polysemy-time, stm, tasty, time
+}:
+mkDerivation {
+  pname = "polysemy-log";
+  version = "0.11.0.0";
+  src = /nix/store/gw84zb1ni89amkmir10g2mp458hbpqan-source;
+  libraryHaskellDepends = [
+    ansi-terminal async base incipit-core polysemy polysemy-conc
+    polysemy-time stm time
+  ];
+  testHaskellDepends = [
+    base incipit-core polysemy polysemy-conc polysemy-plugin
+    polysemy-test polysemy-time tasty time
+  ];
+  benchmarkHaskellDepends = [
+    base incipit-core polysemy polysemy-conc polysemy-plugin
+  ];
+  homepage = "https://github.com/tek/polysemy-log#readme";
+  description = "Polysemy effects for logging";
+  license = "BSD-2-Clause-Patent";
+}
+;
+}
+;
 };
 lower = {
   OneTuple = {
