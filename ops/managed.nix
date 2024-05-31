ad65f05660a42aa7388b46ed67b92bd721a57f18
Torsten Schmits <git@tryp.io>
2024-05-31 18:54:57 +0200

    use managed dependencies

---
+++ ops/managed.nix
@@ -0,0 +1,78 @@
+{
+  bounds = {
+    prelate = {
+      aeson = {
+        lower = null;
+        upper = null;
+      };
+      base = {
+        lower = null;
+        upper = null;
+      };
+      exon = {
+        lower = null;
+        upper = null;
+      };
+      extra = {
+        lower = null;
+        upper = null;
+      };
+      generic-lens = {
+        lower = null;
+        upper = null;
+      };
+      incipit = {
+        lower = null;
+        upper = null;
+      };
+      microlens = {
+        lower = null;
+        upper = null;
+      };
+      microlens-ghc = {
+        lower = null;
+        upper = null;
+      };
+      polysemy-chronos = {
+        lower = null;
+        upper = null;
+      };
+      polysemy-conc = {
+        lower = null;
+        upper = null;
+      };
+      polysemy-log = {
+        lower = null;
+        upper = null;
+      };
+      polysemy-process = {
+        lower = null;
+        upper = null;
+      };
+      polysemy-resume = {
+        lower = null;
+        upper = null;
+      };
+      polysemy-time = {
+        lower = null;
+        upper = null;
+      };
+      template-haskell = {
+        lower = null;
+        upper = null;
+      };
+    };
+  };
+  versions = {
+    latest = {};
+    lower = {};
+  };
+  initial = {
+    latest = {};
+    lower = {};
+  };
+  overrides = {
+    latest = {};
+  };
+  resolving = false;
+}
