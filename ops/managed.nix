0f004f25db1ff62c4342ff7f5b7287ddd67d7ae8
tek <tek@users.noreply.github.com>
2024-05-31 18:20:00 +0000

    Bump 15 dependencies
    New versions:
    
    * aeson-2.2.1.0 [<2.3]
    * base-4.19.1.0 [<4.20]
    * exon-1.7.0.0 [<1.8]
    * extra-1.7.16 [<1.8]
    * generic-lens-2.2.2.0 [<2.3]
    * incipit-0.10.0.0 [<0.11]
    * microlens-0.4.13.1 [<0.5]
    * microlens-ghc-0.4.14.3 [<0.5]
    * polysemy-chronos-0.6.0.2 [<0.7]
    * polysemy-conc-0.13.0.1 [<0.14]
    * polysemy-log-0.11.0.0 [<0.12]
    * polysemy-process-0.13.0.1 [<0.14]
    * polysemy-resume-0.8.0.1 [<0.9]
    * polysemy-time-0.6.0.2 [<0.7]
    * template-haskell-2.21.0.0 [<2.22]

--- ops/managed.nix
+++ ops/managed.nix
@@ -3,68 +3,84 @@
     prelate = {
       aeson = {
         lower = null;
-        upper = null;
+        upper = "2.3";
       };
       base = {
         lower = null;
-        upper = null;
+        upper = "4.20";
       };
       exon = {
         lower = null;
-        upper = null;
+        upper = "1.8";
       };
       extra = {
         lower = null;
-        upper = null;
+        upper = "1.8";
       };
       generic-lens = {
         lower = null;
-        upper = null;
+        upper = "2.3";
       };
       incipit = {
         lower = null;
-        upper = null;
+        upper = "0.11";
       };
       microlens = {
         lower = null;
-        upper = null;
+        upper = "0.5";
       };
       microlens-ghc = {
         lower = null;
-        upper = null;
+        upper = "0.5";
       };
       polysemy-chronos = {
         lower = null;
-        upper = null;
+        upper = "0.7";
       };
       polysemy-conc = {
         lower = null;
-        upper = null;
+        upper = "0.14";
       };
       polysemy-log = {
         lower = null;
-        upper = null;
+        upper = "0.12";
       };
       polysemy-process = {
         lower = null;
-        upper = null;
+        upper = "0.14";
       };
       polysemy-resume = {
         lower = null;
-        upper = null;
+        upper = "0.9";
       };
       polysemy-time = {
         lower = null;
-        upper = null;
+        upper = "0.7";
       };
       template-haskell = {
         lower = null;
-        upper = null;
+        upper = "2.22";
       };
     };
   };
   versions = {
-    latest = {};
+    latest = {
+      aeson = "2.2.1.0";
+      base = "4.19.1.0";
+      exon = "1.7.0.0";
+      extra = "1.7.16";
+      generic-lens = "2.2.2.0";
+      incipit = "0.10.0.0";
+      microlens = "0.4.13.1";
+      microlens-ghc = "0.4.14.3";
+      polysemy-chronos = "0.6.0.2";
+      polysemy-conc = "0.13.0.1";
+      polysemy-log = "0.11.0.0";
+      polysemy-process = "0.13.0.1";
+      polysemy-resume = "0.8.0.1";
+      polysemy-time = "0.6.0.2";
+      template-haskell = "2.21.0.0";
+    };
     lower = {};
   };
   initial = {
@@ -72,7 +88,28 @@
     lower = {};
   };
   overrides = {
-    latest = {};
+    latest = {
+      exon = {
+        version = "1.7.0.0";
+        hash = "142i8ka6b16ydllhhb2305ml3hij66h6y555fp6cvc82166kdrhb";
+      };
+      extra = {
+        version = "1.7.16";
+        hash = "0y27q0zas66qbgvjga0w8dmfjcs0kcn9nwps17iyd60ij3yqivhj";
+      };
+      incipit = {
+        version = "0.10.0.0";
+        hash = "1r3y2wp8wz1ii28a6wb76z6w3sgiah158kwsadrr13w6iryhq047";
+      };
+      microlens-ghc = {
+        version = "0.4.14.3";
+        hash = "0pivw9yrsbfand99kqx8rmll0653k2fzymd6y8av44ky4j3pydga";
+      };
+      polysemy-log = {
+        version = "0.11.0.0";
+        hash = "1phzwj2ig0vx1anscg3qv53ysa0f7gks37pc8gfkh2aws3qp6cda";
+      };
+    };
   };
   resolving = false;
 }
